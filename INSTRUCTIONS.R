d73f1a412484ab42449cd7cfde726c24a6a615f5
OVVO-Financial <ovvo.financialsystems@gmail.com>
2016-06-18 23:43:29 -0400

    Update INSTRUCTIONS.R

--- INSTRUCTIONS.R
+++ INSTRUCTIONS.R
@@ -25,18 +25,18 @@ require(DEoptim)
 #Step4: Optimize Data & Portfolio Construction
   LPM.degree <<- 3;UPM.degree<<- 2;
   LPM.target = 1.00005;UPM.target=1.00005;
-  Conditioned.port=VN.Lin.opt(Conditioned,degree_LPM = LPM.degree,degree_UPM = UPM.degree,target_LPM=LPM.target,target_UPM=UPM.target);
+  Conditioned.port=VN.Lin.opt(Conditioned);
   Conditioned.weights <<- Conditioned.port;
   noquote(paste("Conditioned.port=",Conditioned.port,sep = ""))
     ### COPY & PASTE OUTPUT FROM --> Conditioned.port
   
   
-  Conditioned.no.vol.port= VN.Lin.opt(Conditioned.no.vol,degree_LPM = LPM.degree,degree_UPM = UPM.degree,target_LPM=LPM.target,target_UPM=UPM.target);
+  Conditioned.no.vol.port= VN.Lin.opt(Conditioned.no.vol);
   Conditioned.no.vol.weights <<- Conditioned.no.vol.port;
   noquote(paste("Conditioned.no.vol.port=",Conditioned.no.vol.port,sep = ""))
     ### COPY & PASTE OUTPUT FROM --> Conditioned.no.vol.port
       
-  UN.Conditioned.port=VN.Lin.opt(Raw,degree_LPM = LPM.degree,degree_UPM = UPM.degree,target_LPM=LPM.target,target_UPM=UPM.target);
+  UN.Conditioned.port=VN.Lin.opt(Raw);
   UN.Conditioned.weights <<- UN.Conditioned.port;
   noquote(paste("UN.Conditioned.port=",UN.Conditioned.port,sep = ""))
     ### COPY & PASTE OUTPUT FROM --> UN.Conditioned.port
