c526c0984cd7a41e51debd83116cc09bdbbac0cf
OVVO-Financial <ovvo.financialsystems@gmail.com>
2016-06-21 12:31:36 -0400

    Update Raw_Data_Download.R

--- R/Raw_Data_Download.R
+++ R/Raw_Data_Download.R
@@ -1,6 +1,5 @@
 Raw.Data <- function(Symbols,start.date,end.date){
-    
-  
+
     Data <- new.env()
     
     getSymbols(c(Symbols), from=start.date,to=end.date,env=Data)
@@ -9,21 +8,21 @@ Raw.Data <- function(Symbols,start.date,end.date){
     Returns <- eapply(Data, function(s) ROC(Ad(s), type="discrete"))
     Volume <- eapply(Data, function(s) Vo(s))
     
-    ReturnsDF <<- as.data.frame(do.call(merge, na.omit(Returns)))
-    VolumeDF <<- as.data.frame(do.call(merge, na.omit(Volume)))
+    ReturnsDF <<- as.data.frame(do.call(merge, (Returns)))
+    VolumeDF <<- as.data.frame(do.call(merge, (Volume)))
+
 
 ### Name columns of data.frame    
     colnames(ReturnsDF) <- gsub(".Adjusted","",colnames(ReturnsDF))
     
     
 ### Eliminates securities without complete return series
-    ReturnsDF <- ReturnsDF[,colSums(is.na(ReturnsDF))<2]
     VolumeDF <- VolumeDF[,colSums(is.na(ReturnsDF))<2]
+    ReturnsDF <- ReturnsDF[,colSums(is.na(ReturnsDF))<2]
+    
     
 ### First row of differences generated NA, need to eliminate
     ReturnsDF <<- (ReturnsDF[-1,])
     VolumeDF <<- (VolumeDF[-1,])
     
-    
-    
-  }
+}
